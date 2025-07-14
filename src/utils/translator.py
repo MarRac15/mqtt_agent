b6e198341233c7975489ecb8814fa54a1a716438
Marek <marekraczkowksi5@gmail.com>
2025-07-14 12:10:17 +0200

    error handling for converting the messages

--- src/utils/translator.py
+++ src/utils/translator.py
@@ -2,9 +2,21 @@ import ast
 #translates model data from TTS to kafka
 
 def convert_message(mqtt_message: str):
-    mqtt_message = ast.literal_eval(mqtt_message)
-    old_message = mqtt_message.copy()
-    old_message.pop("ID")
-    new_msg_values = list(old_message.values())
-    kafka_message = {mqtt_message["ID"]: new_msg_values}
-    return kafka_message
+
+    try:
+        if not mqtt_message:
+            return None
+                
+        mqtt_message = ast.literal_eval(mqtt_message)
+        old_message = mqtt_message.copy()
+        old_message.pop("ID")
+        new_msg_values = list(old_message.values())
+        kafka_message = {mqtt_message["ID"]: new_msg_values}
+
+        return kafka_message
+    
+    except Exception as e:
+        
+        print(f"Error while converting the message: {e}")
+        return None
+        
